bd59dd69ec188f69d828061167fed671ea07875f
huy0312 <amshuuhuy10@gmail.com>
2024-06-29 11:30:36 +0700

    Minor Update : Add "Error Message" for Forgot Password

--- src/main/java/com/huynguyen/springsecurity2/controller/ForgotPasswordController.java
+++ src/main/java/com/huynguyen/springsecurity2/controller/ForgotPasswordController.java
@@ -4,12 +4,14 @@ import com.huynguyen.springsecurity2.entity.User;
 import com.huynguyen.springsecurity2.repository.UserRepository;
 import com.huynguyen.springsecurity2.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ForgotPasswordController {
@@ -26,9 +28,11 @@ public class ForgotPasswordController {
     }
 
     @PostMapping("/reset-password-request")
-    public String resetPasswordRequest(@RequestParam("email") String email, HttpServletRequest request) {
+    public String resetPasswordRequest(@RequestParam("email") String email, HttpServletRequest request, RedirectAttributes ra) {
         User user = userService.findByEmail(email);
         if (user == null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("error", "User not found");
             return "redirect:/forgot-password?error=User not found";
         }
         request.getSession().setAttribute("user", user);
